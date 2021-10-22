f65b00d54de7484296f4f4d7cbae89c123a2af10
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-21 20:27:46 -0700

    api.js edits and debugging attempt

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,31 +1,12 @@
-const notes = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 
 
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) console.log("error");
 
+    let notes = JSON.parse(data);
+    notes = console.log(data);
+})
 
-
-
-
-// notes.get("/", (req, res) => {
-//     const { title, text } = req.body;
-
-//     if (title && text) {
-//         const newNote = {
-//             title,
-//             text,
-//         };
-
-//         const response = {
-//             status: "New Note Added",
-//             body: newNote,
-//         };
-
-//         res.json(response);
-//     } else {
-//         res.json("Note not added.");
-//     }
-// });
-
-module.exports = notes;
+// module.exports = app;
