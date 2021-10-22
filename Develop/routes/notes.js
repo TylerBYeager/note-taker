a216b4e4a626e1cab9bde8485c47770a55f6d742
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-21 22:10:20 -0700

    add show db.json

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,43 +1,38 @@
 const app = require("express").Router();
 const fs = require("fs");
 const path = require("path");
+const notes = require("../db/db.json");
 
 
-fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) console.log("error");
-
-    let notes = JSON.parse(data);
-    console.log(notes);
-
-    app.get("/api/notes", function(req, res) {
-        res.json(notes);
-    });
-
-    app.post("/api/notes", function(req, res) {
-        let newNote = req.body;
-        notes.push(newNote);
-        console.log(notes);
-        appendNotes();
-        return notes;
-    });
-
-    app.get("/api/notes/:id", function(req, res) {
-        res.json(notes[req.params.id]);
-    });
-
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
-
-    // app.post("/notes", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/notes.html"));
-    // });
-
-    function appendNotes() {
-        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) console.log("error");
-            return true;
-        })
-    }
+app.get("/notes", function (req, res) {
+    res.json(notes);
 });
+
+app.post("/notes", function (req, res) {
+    let newNote = req.body;
+    notes.push(newNote);
+    console.log(notes);
+    appendNotes();
+    return notes;
+});
+
+app.get("/notes/:id", function (req, res) {
+    res.json(notes[req.params.id]);
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// app.post("/notes", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/notes.html"));
+// });
+
+function appendNotes() {
+    fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+        if (err) console.log("error");
+        return true;
+    })
+};
+
 module.exports = app;
