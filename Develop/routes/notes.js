bea9f0cebe463c627191ca07d8dfa88f2053c77b
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-21 21:43:44 -0700

    remove browser GET error

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -16,14 +16,22 @@ fs.readFile("db/db.json", "utf8", (err, data) => {
     app.post("/api/notes", function(req, res) {
         let newNote = req.body;
         notes.push(newNote);
+        console.log(notes);
         appendNotes();
+        return notes;
     });
 
     app.get("/api/notes/:id", function(req, res) {
         res.json(notes[req.params.id]);
     });
 
-    
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    // app.post("/notes", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/notes.html"));
+    // });
 
     function appendNotes() {
         fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
